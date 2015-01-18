583664644b33e7985a1a78fbd8f7b958297a86db
Charles Dong <charlie.j.dong@gmail.com>
2015-01-17 23:14:05 -0800

    Initial Commit

---
+++ ShareBear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareBear
+//
+//  Created by Charles Dong on 1/17/15.
+//  Copyright (c) 2015 Charles Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
