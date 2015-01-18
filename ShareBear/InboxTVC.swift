2c6ec08478c45f1c8ed7981c6d25d6862219325e
Charles Dong <charlie.j.dong@gmail.com>
2015-01-18 13:56:51 -0800

    Added InboxTVC

---
+++ ShareBear/InboxTVC.swift
@@ -0,0 +1,15 @@
+//
+//  InboxTVC.swift
+//  ShareBear
+//
+//  Created by Charles Dong on 1/18/15.
+//  Copyright (c) 2015 Charles Dong. All rights reserved.
+//
+
+import Foundation
+
+class InboxTVC: UITableViewController {
+
+    
+    
+}
